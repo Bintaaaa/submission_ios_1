f02623ab276f74dede1953f0a2ea11a8edc3b7ad
Mohammad Bijantium Sinatria <79687063+Bintaaaa@users.noreply.github.com>
2022-06-15 10:45:20 +0700

    Initial Commit

---
+++ ios_submission/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_submission
+//
+//  Created by Bijantyum on 15/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
